9f2f5bbcbd1c38e219d9f5a9fc067d2b211ef046
Sergey Pashnin <s.pashnin@encry.ru>
2019-08-09 13:53:52 +0300

    Подключил корс, реализовал получение данных.

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,18 @@
-
 import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
-import {saveCourse} from './save-course.route';
+import { Application } from 'express';
+import { getAllCourses, getCourseById } from './get-courses.route';
+import { searchLessons } from './search-lessons.route';
+import { saveCourse } from './save-course.route';
+const cors = require('cors');
+
 
 const bodyParser = require('body-parser');
 
 const app: Application = express();
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.route('/api/courses').get(getAllCourses);
 
@@ -20,9 +23,8 @@ app.route('/api/lessons').get(searchLessons);
 app.route('/api/courses/:id').put(saveCourse);
 
 
-
-const httpServer = app.listen(9000, () => {
-    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+const httpServer = app.listen(3000, () => {
+  console.log('HTTP REST API Server running at http://localhost:' + httpServer.address().port);
 });
 
 
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,35 +1,61 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
+  noop,
+  Observable,
 } from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
 
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+  // ngOnInit() {
+  //   // todo создаем поток из данных, которые получаем с сервера (c fetch);
+  //   const http$ = new Observable(observer => {
+  //     fetch('http://localhost:9000/api/courses')
+  //       .then(response => {
+  //         return response.json();
+  //       })
+  //       .then(body => {
+  //         observer.next(body);
+  //         observer.complete();
+  //       })
+  //       .catch(error => {
+  //         observer.error(error);
+  //       });
+  //   });
+  //
+  //   http$.subscribe(
+  //     res => console.log('File: about.component.ts, Line - 49, res', res),
+  //     noop,
+  //     () => console.log('File: about.component.ts, Line - 51, `complated`', `complated`),
+  //   );
+  // } // ngOnInit
 
+  ngOnInit() {
 
-    }
-
+    const http$ = new Observable(observer => {
+      fetch('http://localhost:3000/api/courses')
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        });
+    });
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
+  }
 
 }
 
